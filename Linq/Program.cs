b58ba35758639c0a4ba887037cbeec534ffffb36
Artem Vasilev <artvasdmi@gmail.com>
2021-05-30 12:49:45 +0300

    Second commi from Visual Studio

--- Linq/Program.cs
+++ Linq/Program.cs
@@ -1,12 +1,25 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
 
 namespace Linq
 {
     class Program
-    {
+    {   
+        public static void LinqQuery()
+        {
+            int[] myInt = { 10, 20, 40, 50, 80, 75, 66, 30, 32,};
+            var result = from i in myInt where i < 25 orderby i select i;
+            foreach (var item in result)
+            {
+                Console.WriteLine(item);
+            }
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            LinqQuery();
         }
     }
 }
