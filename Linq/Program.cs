af735155c8f7e72cbfed952716719dd280663c42
Artem Vasilev <artvasdmi@gmail.com>
2021-05-30 20:26:32 +0300

    Last commit today

--- Linq/Program.cs
+++ Linq/Program.cs
@@ -5,19 +5,7 @@ using System.Linq;
 
 namespace Linq
 {
-    public class LinqQuery
-    {
-           static int[] myInt = { 10, 20, 40, 50, 80, 75, 66, 30, 32, };
-           IEnumerable<int> result = from i in myInt where i < 25 orderby i select i;
-           public void enumerate()
-           {
-                foreach (var item in result) 
-                { 
-                    Console.WriteLine(item); 
-                }
-                
-           }
-    }
+    
     public class Car
     {
         public string Name { get; set; }
@@ -25,15 +13,48 @@ namespace Linq
         public int Count { get; set; }
         public string Make { get; set; }
     }
+    class ProductInfo
+    {
+        public string Name { get; set; } = "";
+        public string Description { get; set; } = "";
+        public int NumberInStock { get; set; }
+
+        public override string ToString() => $"Name={Name}, " +
+            $"Description={Description}, Number in Stock={NumberInStock}";
+    }
     class Program
-    {   
+    {
+        public static void LinqQuery()
+        {
+            string[] mystring = { "fafawfa", "sgegesg", "sgdsgsgsg", "12345" };
+            int result = (from i in mystring where i.Length > 8 select i).Count();
+            Console.WriteLine(result);
+        }
+        public delegate int Newwest(int a, int b);
+        static void TakeEveryThing(ProductInfo[] prInfo)
+        {
+            var result = from pr in prInfo where pr.NumberInStock > 25 
+                         orderby pr descending
+                         select new {pr.Name, pr.Description };
+            foreach (var item in result)
+            {
+                Console.WriteLine("{0}",item.ToString());
+            }
+        }
+        static Array ReturnArray(ProductInfo[] prInfo)
+        {
+            var result = from pr in prInfo
+                         where pr.NumberInStock > 25
+                         select new { pr.Name, pr.Description };
+            return result.ToArray();
+        }
         static void GetCar(ArrayList myCars)
         {
             var myCarsEnum = myCars.OfType<Car>();
             var hereCar = from c in myCarsEnum
                           where c.Count > 12000 && c.Color == "Green"
                           select c;
-            foreach (var item in hereCar)
+            foreach (var item in hereCar.Reverse())
             {
                 Console.WriteLine("Here: {0} {1}",item.Make, item.Name);
             }
@@ -48,7 +69,7 @@ namespace Linq
                 Console.WriteLine("integer: {0}",item);
             }
         }
-
+      
         static void Main(string[] args)
         {
             ArrayList cars = new ArrayList()
@@ -60,7 +81,30 @@ namespace Linq
                 new Car{Name = "Polo", Color = "White", Count = 11000, Make = "VW"}
             };
 
-            LinqArray();
+            ProductInfo[] itemsInStock = new[]
+            {
+                new ProductInfo{ Name = "Mac's Coffee",
+                                Description = "Coffee with TEETH",
+                                NumberInStock = 24},
+                new ProductInfo{ Name = "Milk Maid Milk",
+                                Description = "Milk cow's love",
+                                NumberInStock = 100},
+                new ProductInfo{ Name = "Pure Silk Tofu",
+                                Description = "Bland as Possible",
+                                NumberInStock = 120},
+                new ProductInfo{ Name = "Cruchy Pops",
+                                Description = "Cheezy, peppery goodness",
+                                NumberInStock = 2},
+                new ProductInfo{ Name = "RipOff Water",
+                                Description = "From the tap to your wallet",
+                                NumberInStock = 100},
+                new ProductInfo{ Name = "Classic Valpo Pizza",
+                                Description = "Everyone loves pizza!",
+                                NumberInStock = 73}
+            };
+            Array result = ReturnArray(itemsInStock);
+            TakeEveryThing(itemsInStock);
+            LinqQuery();
         }
     }
 }
